8687c60b8f872104965aa2d79bd165d164bf7e7a
Beat Seeliger <seb@panter.ch>
2020-06-27 01:00:16 +0200

    rumantsch version

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,5 @@
 # Permitted locales available for the application
-I18n.available_locales = [:en, :de, :fr, :it]
+I18n.available_locales = [:en, :de, :fr, :it, :rm]
 
 # Set default locale to something other than :en
 I18n.default_locale = :en
